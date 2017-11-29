eee49c4e67870eb8cc22944343075d6d174aef22
mcbzz <t9046464560@gmail.com>
2017-11-29 22:35:45 +0800

    Almost done

--- js/script.js
+++ js/script.js
@@ -23,6 +23,29 @@ span.onclick = function () {
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function (event) {
+    if (event.target == body) {
+        modal.style.display = "none";
+    }
+}
+
+
+document.getElementById("mapLink").addEventListener('click', function (event) {
+    event.preventDefault();
+});
+
+// Get the modal
+var modalMap = document.getElementById('map');
+
+// Get the button that opens the modal
+var btn = document.getElementById("mapLink");
+
+// When the user clicks on the button, open the modal 
+btn.onclick = function () {
+    modalMap.style.display = "block";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
     if (event.target == modal) {
         modal.style.display = "none";
     }
