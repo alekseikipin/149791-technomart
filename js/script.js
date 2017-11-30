4dcdd1843382a58a8c6ff5205c327185d9ac7475
mcbzz <t9046464560@gmail.com>
2017-11-30 22:15:12 +0800

    Almost Done

--- js/script.js
+++ js/script.js
@@ -2,28 +2,22 @@ document.getElementById("writeUs").addEventListener('click', function (event) {
     event.preventDefault();
 });
 
-// Get the modal
 var modal = document.getElementById('writeUsModal');
 
-// Get the button that opens the modal
-var a = document.getElementById("writeUs");
+var write = document.getElementById("writeUs");
 
-// Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
 
-// When the user clicks on the button, open the modal 
-a.onclick = function () {
+write.onclick = function () {
     modal.style.display = "block";
 }
 
-// When the user clicks on <span> (x), close the modal
 span.onclick = function () {
     modal.style.display = "none";
 }
 
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function (event) {
-    if (event.target == body) {
+window.onclick = function(event) {
+    if (event.target == modal) {
         modal.style.display = "none";
     }
 }
@@ -45,7 +39,7 @@ btn.onclick = function () {
 }
 
 // When the user clicks anywhere outside of the modal, close it
-window.onclick = function (event) {
+window.onclick = function(event) {
     if (event.target == modal) {
         modal.style.display = "none";
     }
