641abf9ada582ac89c3e31f3807c777897db9497
mcbzz <t9046464560@gmail.com>
2017-12-01 23:51:12 +0800

    Almost done

--- js/script.js
+++ js/script.js
@@ -1,46 +1,37 @@
-document.getElementById("writeUs").addEventListener('click', function (event) {
-    event.preventDefault();
-});
-
-var modal = document.getElementById('writeUsModal');
-
 var write = document.getElementById("writeUs");
+var modal = document.querySelector(".modal-bg");
+var close = document.getElementById("close");
+var customer = modal.querySelector("[name=customer]");
+var close2 = document.querySelector(".modal-bg");
 
-var span = document.getElementsByClassName("close")[0];
-
-write.onclick = function () {
-    modal.style.display = "block";
-}
-
-span.onclick = function () {
-    modal.style.display = "none";
-}
-
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
-}
-
-
-document.getElementById("mapLink").addEventListener('click', function (event) {
-    event.preventDefault();
+write.addEventListener("click", function(evt) {
+    evt.preventDefault();  
+    modal.classList.add("modal-show");
+    customer.focus();
 });
 
-// Get the modal
-var modalMap = document.getElementById('map');
+close.addEventListener("click", function(evt) {
+    evt.preventDefault();  
+    modal.classList.remove("modal-show");
+});
 
-// Get the button that opens the modal
-var btn = document.getElementById("mapLink");
+close2.addEventListener("click", function() {
+    modal.classList.remove("modal-show");
+});
 
-// When the user clicks on the button, open the modal 
-btn.onclick = function () {
-    modalMap.style.display = "block";
-}
 
-// When the user clicks anywhere outside of the modal, close it
+// map
+var mapBtn = document.querySelector(".map-img");
+var map = document.getElementById("map");
+
+mapBtn.addEventListener("click", function(evt) {
+    evt.preventDefault();  
+    map.classList.add("modal-show");
+});
+
 window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
+    if (event.target == map) {
+        map.classList.remove("modal-show");
     }
-}
+};
+
